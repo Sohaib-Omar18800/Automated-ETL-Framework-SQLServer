b832e9488a31e3eb59c42c6611b2d34f35ed4385
Sohiab Omar <sohaibomar18800@gmail.com>
2026-01-15 16:16:17 +0200

    Create start_gold.py
    Script to start etl from silver to gold

---
+++ src/database/start_gold.py
@@ -0,0 +1,14 @@
+def start_gold():
+    # [database, connection_url, connection_url_alchemy, servername]
+    user_info = sql_server_var()
+    con = dk.connect(f'{user_info[0]}.duckdb')
+    tables = table()
+    for table_names in tables.values():
+        names = table_names[0]
+        df_query = f"""
+SELECT * FROM silver.{names}
+"""
+        df = pl.read_database_uri(df_query, uri=user_info[2])
+        con.register(f'temp_{names}', df)
+    list_fact = create_gold(con=con)
+    return [user_info[0], list_fact]
